ea1c42f08dda1b5433661143b66126d8be800c3d
cwgordon7 <cwgordon7@cwgordon.com>
2011-06-18 16:45:48 -0400

    Issue #959518 by cwgordon7, kanani: Port to Drupal 7.

--- talkpage.tpl.php
+++ talkpage.tpl.php
@@ -1,27 +1,20 @@
-<?php // $Id$ ?>
 <?php
 /**
  * @file
  * Overridable template file for talk page.
  *
  * Available variables:
+ *   $comment_count represents the number of comments on the node.
+ *   $comments represents the rendered comments, form, and/or link.
  *   $node represents the node whose comments we're displaying.
- *   $comments represents the rendered comments.
- *   $comment_link represents an "add new comment" link.
- *   $add_comments is TRUE if the user has permission to add comments.
- *   $redisplay is TRUE if the "add new comment" link should be redisplayed at the bottom of the page.
  *   $title represents the title of the talk page. Defaults to "Talk".
  */
 ?>
-<?php if ($comment_link): ?>
-  <p>
-    <?php print $comment_link; ?>
-  </p>
-  <br />
-<?php endif; ?>
-<?php print $comments; ?>
-<?php if ($comment_link && $redisplay): ?>
-  <p>
-  <?php print $comment_link; ?>
-  </p>
-<?php endif; ?>
+<div class="talk-page">
+  <?php if ($comment_count == 0): ?>
+    <div class="talk-page-empty-message">
+      <?php print t('There are currently no comments on this page.'); ?>
+    </div>
+  <?php endif; ?>
+  <?php print $comments; ?>
+</div>
